456232c626197e68a1a1eb52961afbb657521d7f
DevelWanderer <52003425+DevelWanderer@users.noreply.github.com>
2019-06-27 15:25:29 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -6,33 +6,32 @@ $channal_secret = '576c647ae353642081fe5c8fa4826f80';
 
 // Get POST body content
 $content = file_get_contents('php://input');
-$usname = $channal_secret->getProfile('<userId>');
-if ($usname->isSucceeded()) {
-    $profile = $usname->getJSONDecodedBody();
-}
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-// Loop through each event
-foreach ($events['events'] as $event) {
-// Reply only when message sent is in 'text' format
-if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-// Get text sent
-$text = $event['source']['userId'];
-// Get replyToken
-$replyToken = $event['replyToken'];
-// Build message to reply back
-$messages = [
-'type' => 'text',
-'text' => $profile['displayname']"สวัสดีครับ"
-];
-// Make a POST Request to Messaging API to reply to sender
+$some = json_decode($content,true);
+
+$Header1 = array();
+$Header1[] = "Authorization:Bearer{$access_token}";
+
+$profile = "https://api.line.me/v2/bot/profile/".&arrJson['event'[0]['source']['userId'];
+                                                          
+$ch = curl_init();
+curl_setopt($ch1,CURLOPT_URL,$profile);
+curl_setopt($ch1,CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch1,CURLOPT_HTTPHEADER,$Header1);
+$result1 = curl_exec($ch1);
+curl_close($ch1);
+
+$some1 = json_decode($result1,true);
+                                                          
 $url = 'https://api.line.me/v2/bot/message/reply';
-$data = [
-'replyToken' => $replyToken,
-'messages' => [$messages],
-];
+                                                          
+$data =array();
+$data['replyToken'] = $some['event'][0]['replyToken'];
+$data['message'][0]['type'] = "text";
+      $data['message'][0]['type'] = $some1['displayname']."สวัสดีครับ";
+
+
+// Make a POST Request to Messaging API to reply to sender
+
 $post = json_encode($data);
 $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 $ch = curl_init($url);
