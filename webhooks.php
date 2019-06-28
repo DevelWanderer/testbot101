bb54d50a603a5df070b61e81a44ca0ded7771e17
DevelWanderer <52003425+DevelWanderer@users.noreply.github.com>
2019-06-28 11:52:44 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -19,9 +19,7 @@ $content = file_get_contents('php://input');
      $arrayPostData['to'] = $id;
      $arrayPostData['messages'][0]['type'] = "text";
      $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
-     $arrayPostData['messages'][1]['type'] = "sticker";
-     $arrayPostData['messages'][1]['packageId'] = "2";
-     $arrayPostData['messages'][1]['stickerId'] = "34";
+     
      pushMsg($arrayHeader,$arrayPostData);
   }
   function pushMsg($arrayHeader,$arrayPostData){
