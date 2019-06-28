7687599ffc9aa55e298d506fbf752ede807c3472
DevelWanderer <52003425+DevelWanderer@users.noreply.github.com>
2019-06-28 12:13:42 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -18,7 +18,7 @@ $content = file_get_contents('php://input');
   //รับ id ของผู้ใช้
   $id = $arrayJson['events'][0]['source']['userId'];
   #ตัวอย่าง Message Type "Text + Sticker"
-  if($message == "สวัสดี"){
+  if($arrayjson['type'] == 'message' && $arrayJson['message']['type'] == 'text'){
      $arrayPostData['to'] = $id;
      $arrayPostData['messages'][0]['type'] = "text";
      $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
