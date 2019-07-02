3c0ce861fbcd9ddeaf23962886920875d4121f05
DevelWanderer <52003425+DevelWanderer@users.noreply.github.com>
2019-07-02 12:11:37 +0700

    Update richmenu.php

--- richmenu.php
+++ richmenu.php
@@ -2,7 +2,7 @@
 // Using shell_exec for better understandability. Use cURL or other http client for production environment.
 require "vendor/autoload.php";
 require_once('vendor/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-$channelAccessToken = 'Z/vaB91Q/WsmdQLWN1UwFl5k6I+fnBwcHZSju9jIshHsZ8NpD5GiGirPc6FQ/wKKwD5qViTXHs66qDThOvCjYez41saC2XWUxmFJAjAzDWNrKWA/xFA1uELYyIFiXKuc5RxgAQxyJLc58FofJTS0GwdB04t89/1O/w1cDnyilFU=';
+
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('Z/vaB91Q/WsmdQLWN1UwFl5k6I+fnBwcHZSju9jIshHsZ8NpD5GiGirPc6FQ/wKKwD5qViTXHs66qDThOvCjYez41saC2XWUxmFJAjAzDWNrKWA/xFA1uELYyIFiXKuc5RxgAQxyJLc58FofJTS0GwdB04t89/1O/w1cDnyilFU='));
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('576c647ae353642081fe5c8fa4826f80')]);
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
