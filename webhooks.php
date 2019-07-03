9e0ef0d38191f08410a644a16f8bbede007ac1ab
DevelWanderer <testbot_2122242@hotmail.com>
2019-07-03 16:23:02 +0700

    tb108

--- webhooks.php
+++ webhooks.php
@@ -30,7 +30,16 @@ $content = file_get_contents('php://input');
   {
     if($message == "สวัสดี"){
     //$arrayPostData['to'] = $id;
-    $arrayPostData  = array(
+    $arrayPostData['messages'][0]['type'] = "text";
+    $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา".$name2;
+    $arrayPostData['messages'][1]['type'] = "text";
+    $arrayPostData['messages'][1]['text'] = "เราชื่อดอร่านะ";
+    $arrayPostData['messages'][2]['type'] = "sticker";
+    $arrayPostData['messages'][2]['packageId'] = "2";
+    $arrayPostData['messages'][2]['stickerId'] = "34";
+    $arrayPostData['messages'][3]['type'] = "text";
+    $arrayPostData['messages'][3]['text'] = "https://erp.wealththai.net/quickregister??".$id;
+    $arrayPostData['messages'][4]= array(
       "type" => "button",
       "action"=> array(
         "type"=> "uri",
