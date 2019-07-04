76fd99b91de72bb8e0d25f7d5528af55d73936c2
DevelWanderer <testbot_2122242@hotmail.com>
2019-07-04 16:48:27 +0700

    ะิ

--- webhooks.php
+++ webhooks.php
@@ -32,7 +32,8 @@ $content = file_get_contents('php://input');
 
     $arrayPostData['to'] = $id;
     $arrayPostData['messages'][0]['type'] = "text";
-    $arrayPostData['messages'][0]['text'] = "สวัสดีคุณ ".$name1."\n"."มีข้อความใหม่ส่งถึงคุณ"."\n"."ส่งมาจาก ".$sendername."\n"."ข้อความ ".$messagesend;
+    $arrayPostData['messages'][0]['text'] = "สำหรับคนที่ยังไม่ได้สมัครเป็นสมาชิก Wealththai กดลิ้งนี้"."https://erp.wealththai.net/quickregister?lusid?".$id."?lusid?"."\n".
+    "สำหรับคนที่เป็นสมาชิก Wealththai แล้วกดลิ้งนี้"."https://erp.wealththai.net/quickregister?lusid?".$id."?lusid?"."\n"
   pushMsg($arrayHeader,$arrayPostData);
 
     //!empty == "!=" (if($_POST['username'] != NULL))
