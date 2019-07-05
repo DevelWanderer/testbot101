870b1faa54ca148cfc4cc2c369b97d1ed69e5f8f
DevelWanderer <testbot_2122242@hotmail.com>
2019-07-05 12:15:10 +0700

    tb

---
+++ controllers/img.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// import the Intervention Image Manager Class
+use Intervention\Image\ImageManager;
+
+class Img extends CI_Controller {
+
+    public function __construct()
+    {
+            parent::__construct();
+
+    }
+    public function index()
+    {
+        // สร้างตัวแปรอ้างอิง object ตัวจัดการรูปภาพ
+        $manager = new ImageManager();
+
+        // ทดสอบสร้างรูปขนาด 200x200 พื้นหลังสีแดง
+        $img = $manager->canvas(200, 200, '#ff0000');
+
+        // ส่ง HTTP header และข้อมูลของรูปเพื่อนำไปแสดง
+        echo $img->response();
+
+    }  
+
+}
