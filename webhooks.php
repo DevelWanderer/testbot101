a7294dd6201c7d56e14c5e7b286cd05263886e8a
DevelWanderer <testbot_2122242@hotmail.com>
2019-07-05 14:07:37 +0700

    tb

--- webhooks.php
+++ webhooks.php
@@ -49,7 +49,7 @@ if(!empty($_POST['passwordconnecttoline']))
 {
   if($_POST['passwordconnecttoline'] == 'Connect1562Server')
   {
-    $arrayPostData['to'] = $_POST['lineid'];
+    $arrayPostData['to'] = 'Uaf703e9707ecc91b4ebb7f6bc758ee1a';
     $arrayPostData['messages'][0]['type'] = "text";
     $arrayPostData['messages'][0]['text'] = 'เชื่อมต่อไลน์กับบัญชีระบบ Wealththai ของคุณเรียบร้อยแล้ว!';
     pushMsg($arrayHeader,$arrayPostData);
