38a0b39ad97293b3ad0200a2db2ca16e882522ce
DevelWanderer <testbot_2122242@hotmail.com>
2019-07-05 12:40:48 +0700

    tb

--- controllers/img.php
+++ controllers/img.php
@@ -1,5 +1,5 @@
 <?php
-defined('testbot101/tree/master/controllers')  OR exit('No direct script access allowed');
+defined('testbot101/blob/master/controllers/')  OR exit('No direct script access allowed');
  ;
 
 // import the Intervention Image Manager Class
