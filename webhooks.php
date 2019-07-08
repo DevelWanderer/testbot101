60ca01f15a5934d08fc8318462a2a32df298768a
DevelWanderer <testbot_2122242@hotmail.com>
2019-07-08 11:38:07 +0700

    tb

--- webhooks.php
+++ webhooks.php
@@ -100,7 +100,7 @@ function replyMsg($arrayHeader,$arrayReplyData){
      curl_setopt($ch, CURLOPT_HEADER, false);
      curl_setopt($ch, CURLOPT_POST, true);
      curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
-     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayReplyData));
      curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
      $result = curl_exec($ch);
@@ -113,7 +113,7 @@ function replyMsg($arrayHeader,$arrayReplyData){
    curl_setopt($ch, CURLOPT_HEADER, false);
    curl_setopt($ch, CURLOPT_POST, true);
    curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
-   curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+   curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPushData));
    curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
    $result = curl_exec($ch);
