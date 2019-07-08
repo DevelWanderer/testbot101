193e0dfdf5f123f712c773fe529b814db6d4ec81
DevelWanderer <testbot_2122242@hotmail.com>
2019-07-08 12:07:33 +0700

    tb

--- webhookss.php
+++ webhookss.php
@@ -38,7 +38,7 @@ $content = file_get_contents('php://input');
         $arrayPostData['messages'][0]['actions'][3]['area'][2]['width'] ="520";
         $arrayPostData['messages'][0]['actions'][4]['area'][3]['height'] ="454";
         $arrayPostData['messages'][0]['actions'][5]['linkUri'] ="https://google.com";
-        replyMsg($arrayHeader,$arrayPostData);
+        pushMsg($arrayHeader,$arrayPostData);
     }
   function replyMsg($arrayHeader,$arrayPostData){
      $strUrl = "https://api.line.me/v2/bot/message/reply";
@@ -53,5 +53,19 @@ $content = file_get_contents('php://input');
      $result = curl_exec($ch);
      curl_close ($ch);
   }
+
+/*function pushMsg($arrayHeader,$arrayPostData){
+ $strUrl = "https://api.line.me/v2/bot/message/push";
+ $ch = curl_init();
+ curl_setopt($ch, CURLOPT_URL,$strUrl);
+ curl_setopt($ch, CURLOPT_HEADER, false);
+ curl_setopt($ch, CURLOPT_POST, true);
+ curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+ curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+ curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+ $result = curl_exec($ch);
+ curl_close ($ch);
+}*/
   exit;
 ?>
