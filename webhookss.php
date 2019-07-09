a61bce3a8f09b00211639ee100187449f69a78b8
DevelWanderer <testbot_2122242@hotmail.com>
2019-07-09 15:19:46 +0700

    tb

--- webhookss.php
+++ webhookss.php
@@ -3,6 +3,18 @@ require "vendor/autoload.php";
 require_once('vendor/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 $access_token = 'YmOTeNtLzS70P55TfovHyurPZc0jBcUGR4GSFlEqzJQmCbtsoAOurD6SFUbRG8MvHaAQV3gF/1Fj29KWMkHEpIQuUS1Wn4p18JW2Mjx4ky0XxqUgTVJ/x1qR9CR7UwuQ854y0cJhethnu3CPfPT9XQdB04t89/1O/w1cDnyilFU=';
 
+use LINE\LINEBot;
+use LINE\LINEBot\Constant\ActionType;
+use LINE\LINEBot\Constant\MessageType;
+use LINE\LINEBot\ImagemapActionBuilder\AreaBuilder;
+use LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder;
+use LINE\LINEBot\ImagemapActionBuilder\ImagemapUriActionBuilder;
+use LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder;
+use LINE\LINEBot\MessageBuilder\Imagemap\VideoBuilder;
+use LINE\LINEBot\MessageBuilder\Imagemap\ExternalLinkBuilder;
+use LINE\LINEBot\MessageBuilder\ImagemapMessageBuilder;
+use LINE\Tests\LINEBot\Util\DummyHttpClient;
+use PHPUnit\Framework\TestCase;
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -37,15 +49,16 @@ $content = file_get_contents('php://input');
         $arrayReplyData['messages'][0]['baseSize']['height'] = "1040";
         $arrayReplyData['messages'][0]['action'][0]['type'] = "uri";
         $arrayReplyData['messages'][0]['action'][0]['linkUri']  = "https://google.com";
-        $arrayReplyData['messages'][0]['action'][0]['area'][0]['x'] = "16";
-        $arrayReplyData['messages'][0]['action'][0]['area'][0]['y'] = "18";
-        $arrayReplyData['messages'][0]['action'][0]['area'][0]['width'] = "1006";
-        $arrayReplyData['messages'][0]['action'][0]['area'][0]['height'] = "1001";
+        $arrayReplyData['messages'][0]['action'][0]['area']['x'] = "16";
+        $arrayReplyData['messages'][0]['action'][0]['area']['y'] = "18";
+        $arrayReplyData['messages'][0]['action'][0]['area']['width'] = "1006";
+        $arrayReplyData['messages'][0]['action'][0]['area']['height'] = "1001";
 
 
 
         replyMsg($arrayHeaderr,$arrayReplyData);
     }
+
     elseif($message == "แมว"){
 
       $arrayReplyData['replyToken'] = $arrayJson['events'][0]['replyToken'];
