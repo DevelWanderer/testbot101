b63e4e5208ac9ca8269035c74034b7f198e984b8
DevelWanderer <testbot_2122242@hotmail.com>
2019-07-13 11:43:40 +0700

    tb

--- webhookss.php
+++ webhookss.php
@@ -13,7 +13,7 @@ require_once ('vendor/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
     //รับข้อความจากผู้ใช้
     $message = $arrayJson['events'][0]['message']['text'];
 #ตัวอย่าง Message Type "Text"
-    if($message == "hi"){
+    if($message == "test"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
         $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
